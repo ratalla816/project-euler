5b8e17c924cd3d96112b7d1aafd8ff38a74cfc6e
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2022-08-23 21:31:50 -0500

    solved

--- evenFibonacciNumbers.js
+++ evenFibonacciNumbers.js
@@ -7,3 +7,32 @@ By starting with 1 and 2, the first 10 terms will be:
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 */
 
+let evenSum = (limit) => {
+
+    let sum = 0
+
+    let a = 1
+    let b = 2
+    console.log(a)
+    console.log(b)
+    if(limit >= 2) {
+        sum = sum + b
+    }
+    let c = 0 
+    while(c <= limit){
+     c = a + b 
+     console.log(c)
+     if (c % 2 === 0) {
+        sum = sum + c
+    }
+
+    a = b 
+    b = c
+
+}
+
+    return sum 
+
+}
+
+console.log('Result is ' + evenSum(100));
