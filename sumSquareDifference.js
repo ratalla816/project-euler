2f76a6dcdcdace7d8eab7cf418694be1c5f14846
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2022-08-27 18:32:51 -0500

    solved

---
+++ sumSquareDifference.js
@@ -0,0 +1,36 @@
+/*
+The sum of the squares of the first ten natural numbers is,
+
+            1^2 + 2^2 +...+10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+
+            (1 + 2+...+10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+
+            3025 - 385 = 2640
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+let sumSquareDifference = (n) => {
+
+    let squaredSum = 0
+    let sum = 0
+    let i 
+    for (i = 1; i <= n; i++) {
+        sum = sum + i 
+
+        squaredSum = squaredSum + Math.pow(i, 2)
+    }
+
+    console.log('sum of each number squared is ' + squaredSum)
+
+    let sumSquared = Math.pow(sum, 2)
+    console.log('(sum of all numbers) squared is ' + sumSquared)
+
+    return sumSquared - squaredSum
+}
+
+console.log('result is ' + sumSquareDifference(10));
