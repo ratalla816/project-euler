1ea50f671ac206a13080051a2862a5412ecfe2ac
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2022-08-28 16:32:14 -0500

    solved

--- 015-latticePaths.js
+++ 015-latticePaths.js
@@ -5,3 +5,30 @@ Starting in the top left corner of a 2×2 grid, and only being able to move to t
 How many such routes are there through a 20×20 grid?
 */
 
+
+let factorial = (number) => {
+    if (number === 1) {
+        return number
+
+    } else {
+        return number * factorial(number - 1)
+    }
+}
+
+let latticePaths = (gridSize) => {
+
+let n = 2 * gridSize 
+let k = gridSize
+
+return factorial(n) / (factorial(k) * factorial (n-k))
+
+
+
+
+}
+
+console.log('result is ' + latticePaths(20));
+
+// 4 SHOULD RETURN 70
+// 9 SHOULD RETURN 48620
+// 20 SHOULD RETURN 137846528820
