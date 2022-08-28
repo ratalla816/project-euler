eecd164edabb40f8a585f248895971e2ef105bf2
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2022-08-28 18:32:03 -0500

    solved

--- 007-10001stPrime.js
+++ 007-10001stPrime.js
@@ -6,13 +6,18 @@ What is the 10 001st prime number?
 
 let nthPrime = (n) => {
 
-
+// Number of values generated
     let count = 0
+
+// First Prime Value    
     let candidate = 2
+
+// Latest found Prime    
     let latestPrimeNumber = 2
 
     while(count < n) {
 
+// Assume the value is prime    
         let isPrime = true
         let i
         for(i = 2; i < candidate; i ++) {
@@ -23,11 +28,13 @@ let nthPrime = (n) => {
             }
         }
 
+// If value is still prime, set last prime and update count        
         if(isPrime) {
             latestPrimeNumber = candidate
             count = count + 1
         }
 
+// move to the next number        
         if (candidate === 2) {
             candidate = 3
             
@@ -36,14 +43,11 @@ let nthPrime = (n) => {
         }
     }
 
+// end of cycle    
         return latestPrimeNumber
 }
 
 console.log('result is ' + nthPrime(10001))
-
-
-
-
 
 // 6 SHOULD RETURN 13
 // 10 SHOULD RETURN 29
